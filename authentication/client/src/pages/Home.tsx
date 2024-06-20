f901c620f090440fd3b15d0405173dae5531a6d6
Abhishek Thakur <github@bizdesire.com>
2024-06-20 12:31:34 +0530

    setup-commit

--- authentication/client/src/pages/Home.tsx
+++
@@ -1,6 +0,0 @@
-import Hero from "../components/Hero";
-
-const Home = () => {
-  return <Hero />;
-};
-export default Home;
---
+++ mern/server/server.js
@@ -0,0 +1,30 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Enable CORS for all routes
+app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
